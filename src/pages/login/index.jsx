8be0af62ad9d240917bcf8d51f4f235e32864ce0
alonronin <alon@ronin.co.il>
2019-07-06 01:35:43 +0300

    :wrench: updates from live code

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,23 +5,21 @@ import LogIn from '../../organism/Login';
 import Logo from '../../atoms/Logo';
 import Icon from '../../atoms/Icon';
 
-const Login = ({ store }) => {
-  return (
-    <div className="page">
-      <Logo />
-      <LogIn form={store.form} onSubmit={store.onSubmit} />
+const LogInPage = ({ store }) => (
+  <div className="page">
+    <Logo />
+    <LogIn form={store.form} onSubmit={store.onSubmit} />
 
-      <p className="icons">
-        <Icon facebook />
-        <Icon google />
-      </p>
+    <p className="icons">
+      <Icon facebook />
+      <Icon google />
+    </p>
 
-      <p className="footer">
-        Dont have an account? <br />
-        <a href="">Sign Up</a>
-      </p>
-    </div>
-  );
-};
+    <p className="footer">
+      Don't have account? <br />
+      <a href="">Sign Up</a>
+    </p>
+  </div>
+);
 
-export default observer(Login);
+export default observer(LogInPage);
--- src/pages/login/login.story.jsx
+++ src/pages/login/login.story.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import Login from './';
+import LogInPage from './';
 import Store from './store';
 
 const store = new Store();
+
 store.onSubmit = e =>
   store.form.onSubmit(e, {
     onSuccess(form) {
-      action('Submit')(form.values());
+      action('Success')(form.values());
     },
-
     onError(form) {
       action('Error')(form.errors());
     },
   });
 
-storiesOf('Pages', module).add('Login', () => <Login store={store} />);
+storiesOf('Pages').add('Login', () => <LogInPage store={store} />);
